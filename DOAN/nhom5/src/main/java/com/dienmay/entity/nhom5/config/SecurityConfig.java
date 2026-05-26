649b5a076d6cd992066a7c4939212383858dc4a0
VuongNguyen163 <nguyenvietvuong16032005@gmail.com>
2026-05-26 23:17:47 +0700

    thêm thẻ sản phẩm

--- DOAN/nhom5/src/main/java/com/dienmay/entity/nhom5/config/SecurityConfig.java
+++ DOAN/nhom5/src/main/java/com/dienmay/entity/nhom5/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 "/js/**",
                 "/assets/**",
                 "/images/**",
+                "/uploads/**",
                 "/webjars/**"
             )
             .permitAll()
